d28cce9a24a02f02e351fd0bb71a06547581e63b
Phuoc Nguyen <nguyenphuoc0927@gmail.com>
2022-07-05 22:33:30 +0700

    + Add_new_Cart

--- ECproject/settings.py
+++ ECproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'category',
     'store',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'carts.context_processors.counter',
             ],
         },
     },
--- ECproject/urls.py
+++ ECproject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -24,4 +24,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,name='home'),
+    path('cart/', include('carts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
