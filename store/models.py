1f3c9cbc5208dc172ebfcde0373975a83c717ee3
nhanleno <nhanleno@gmail.com>
2022-07-06 16:22:26 +0700

    nhan

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from category.models import Category
+from django.urls import reverse
 
 class Product(models.Model):
     product_name     = models.CharField(max_length=200,unique=True)
@@ -13,5 +14,23 @@ class Product(models.Model):
     created_date    = models.DateTimeField(auto_now_add=True)
     modified_date   = models.DateTimeField(auto_now=True)
 
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
+
     def __str__(self):
         return self.product_name
+
+variation_category_choice = (
+    ('color', 'color'),
+    ('size' , 'size')
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product
