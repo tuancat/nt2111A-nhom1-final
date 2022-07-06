156faf6747559fb82b10af8a6d8b613e75dd947b
nhanleno <nhanleno@gmail.com>
2022-07-06 16:31:42 +0700

    add class variation

--- ECproject/urls.py
+++ ECproject/urls.py
@@ -24,9 +24,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,name='home'),
-<<<<<<< .mine
-    path('store/',include('store.urls')),
-=======
+    path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
->>>>>>> .theirs
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
