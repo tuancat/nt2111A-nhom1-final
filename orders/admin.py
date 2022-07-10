e5376f031d7223d94df4fce4cdeda10f4a56b969
nhanleno <nhanleno@gmail.com>
2022-07-10 11:19:49 +0700

    modify orders/admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Payment, Order, OrderProduct
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_number','full_name','phone','email','city','order_total','tax','status','is_ordered','created_at']
+    list_filter = ['status','is_ordered']
+    search_fields = ['order_number','first_name','last_name','phone','email']
 
 admin.site.register(Payment)
 admin.site.register(Order)
