e358e41cc8c41d3d294b73e873bda63885fbc9eb
Haruxuan <thanhxuan03071989@gmail.com>
2022-07-11 14:21:25 +0700

    fixloi

--- ECproject/urls.py
+++ ECproject/urls.py
@@ -27,7 +27,6 @@ urlpatterns = [
     path('', views.home,name='home'),
     path('store/',include('store.urls')),
     path('cart/', include('carts.urls')),
-
     # ORDERS
     path('order/',include('orders.urls')),
     
