591f4152fec7ed34a79976b55b164ca1e296d2b4
Haruxuan <thanhxuan03071989@gmail.com>
2022-07-16 21:07:36 +0700

    fix addcart

--- carts/views.py
+++ carts/views.py
@@ -119,5 +119,30 @@ def cart(request, total=0, quantity=0, cart_items=None):
         'cart_items': cart_items,
         'tax': tax,
         'grand_total': grand_total
-    }
+        }
     return render(request, 'store/cart.html', context)
+
+
+@login_required(login_url='login')
+def checkout(request, total=0, quantity=0, cart_items=None):
+    try:
+        tax = 0
+        grand_total = 0
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (0.1 * total)
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass # just ignore
+
+        context = {
+                'total': total,
+                'quantity': quantity,
+                'cart_items': cart_items,
+                'tax': tax,
+                'grand_total': grand_total
+        }
+        return render(request, 'store/checkout.html', context)
