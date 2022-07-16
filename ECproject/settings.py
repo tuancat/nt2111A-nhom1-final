0b13c36af8d4b4ced705cbe1c2268e167089b154
Phuoc Nguyen <nguyenphuoc0927@gmail.com>
2022-07-16 23:36:50 +0700

    +Modify_Cart

--- ECproject/settings.py
+++ ECproject/settings.py
@@ -81,9 +81,12 @@ WSGI_APPLICATION = 'ECproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecproject',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Huuphuoc@0927'
     }
 }
 
