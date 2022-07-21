c23fb8ff4352b67b89fac1f0a0a18eb44e6d79ac
nhanleno <nhanleno@gmail.com>
2022-07-21 19:32:01 +0700

    to payment

--- orders/forms.py
+++ orders/forms.py
@@ -1,8 +1,9 @@
-from  django import forms
+from django import forms
+from django.forms import ModelForm
 from .models import Order
 
 
-class OrderForm(forms.ModelForm):
+class OrderForm(forms.Form):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name','phone', 'email', 'address_line_1', 'address_line_2', 'country', 'state', 'city', 'order_note']
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse,JsonResponse
-from carts.models import CartItem
+from carts.models import CartItem, Cart
 from .forms import OrderForm
 import datetime
 from .models import Order, Payment, OrderProduct
@@ -11,14 +11,16 @@ from django.template.loader import render_to_string
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
 import json
+from accounts.models import Account
 
 # Create your views here.
 
 def payments(request):
-    body = json.loads(request.body)
-    order = Order.objects.get(user=request.user, is_ordered=False, order_number=body['orderID'])
-
+    #body = json.loads(request.body)
+    order = Order.objects.filter(user=request.user)
+    
 # Lưu chi tiết dao dịch
+    '''
     payment = Payment(
         user = request.user,
         payment_id = body['transID'],
@@ -28,7 +30,13 @@ def payments(request):
     )
     payment.save()
 
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+    '''
+
 #Chuyển tất cả số lượng hàng trong giỏ hàng vào table Order Product
+    
     cart_items = CartItem.objects.filter(user=request.user)
 
     for item in cart_items:
@@ -60,7 +68,7 @@ def payments(request):
 
 # Gửi đơn hàng vào mail của khách
     mail_subject = 'Cảm ơn bạn đã đặt hàng của chúng tôi'
-    message = render_to_string('account/order_received_email.html', {
+    message = render_to_string('orders/order_received_email.html', {
         'user': request.user,
         'order': order,
     })
@@ -70,12 +78,12 @@ def payments(request):
     send_email.send()
 
 # Gửi số đơn hàng và giao dịch id
-    data = {
+    '''data = {
         'orders_number': order.order_number,
         'transID': payment.payment_id,
     }
 
-    return JsonResponse(data)
+    return JsonResponse(data)'''
 
    # return render(request, 'orders/payments.html')
 
@@ -88,58 +96,56 @@ def place_order(request, total=0, quantity=0,):
     if cart_count <= 0:
         return redirect('store')
 
-    grand_total = 0
-    tax = 0
-    for cart_item in cart_items:
-        total += (cart_item.product.price * cart_item.quantity)
-        quantity += cart_item.quantity
-    tax = (0.1 * total)
-    grand_total = total + tax
+    else:
+        grand_total = 0
+        tax = 0
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (0.1 * total)
+        grand_total = total + tax
 
 
-    if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            data = Order()
-            data.user = current_user
-            data.first_name = form.cleaned_data['first_name']
-            data.last_name = form.cleaned_data['last_name']
-            data.phone = form.cleaned_data['phone']
-            data.email = form.cleaned_data['email']
-            data.address_line_1 = form.cleaned_data['address_line_1']
-            data.address_line_2 = form.cleaned_data['address_line_2']
-            data.country = form.cleaned_data['country']
-            data.state = form.cleaned_data['state']
-            data.city = form.cleaned_data['city']
-            data.order_note = form.cleaned_data['order_note']
-            data.order_total = grand_total
-            data.tax = tax
-            data.ip = request.META.get('REMOTE_ADDR')
-            data.save()
+    if request.method == "POST":
+        #pform = OrderForm(request.POST)
+        #if pform.is_valid():
+        data = Order()
+        data.user = current_user
+        data.first_name = request.POST.get('first_name')
+        data.last_name = request.POST.get('last_name')
+        data.phone = request.POST.get('phone_number')
+        data.email = request.POST.get('email')
+        data.address_line_1 = request.POST.get('address_line_1')
+        data.address_line_2 = request.POST.get('address_line_2')
+        data.country = request.POST.get('country')
+        data.state = request.POST.get('state')
+        data.city = request.POST.get('city')
+        data.order_note = request.POST.get('order_note')
+        data.order_total = grand_total
+        data.tax = tax
+        data.ip = request.META.get('REMOTE_ADDR')
+        data.save()
 
-            # Tạo mã đơn hàng
-            yr = int(datetime.date.today().strftime('%Y'))
-            dt = int(datetime.date.today().strftime('%d'))
-            mt = int(datetime.date.today().strftime('%m'))
-            d = datetime.date(yr,mt,dt)
-            current_date = d.strftime("%Y%m%d") #20220706
-            order_number = current_date + str(data.id)
-            data.order_number = order_number
-            data.save()
-            return redirect('checkout')
-            #return HttpResponse('Đã đặt hàng thành công')
-
-            '''
-            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
-            context = {
-                'order': order,
-                'cart_items': cart_items,
-                'total': total,
-                'tax': tax,
-                'grand_total': grand_total,
-            }
-            return render(request, 'orders/payments.html', context)
-            '''
+        # Tạo mã đơn hàng
+        yr = int(datetime.date.today().strftime('%Y'))
+        dt = int(datetime.date.today().strftime('%d'))
+        mt = int(datetime.date.today().strftime('%m'))
+        d = datetime.date(yr,mt,dt)
+        current_date = d.strftime("%Y%m%d") #20220706
+        order_number = current_date + str(data.id)
+        data.order_number = order_number
+        data.save()
+        
+        order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
+        context = {
+            'order': order,
+            'cart_items': cart_items,
+            'total': total,
+            'tax': tax,
+            'grand_total': grand_total,
+        }
+        return render(request, 'orders/payments.html', context)
+        
     else:
         return redirect('checkout')
 
